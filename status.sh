eefc19a23b3d8d37650fca09bbca2de67648b336
Jordan Holberg <1364262+EvilJordan@users.noreply.github.com>
2024-02-19 15:54:09 -0800

    Proposer Duty Information

--- status.sh
+++ status.sh
@@ -7,32 +7,50 @@ GENESISTIMESTAMP=1606795200
 NOW=$(date +%s)
 IFS=', ' # array split deliniator
 
+chainStatus=$($ETHDO chain status) # capture output of ethdo chain status to know when an epoch is ending
+epoch=`echo -e $chainStatus | grep -oEm 2 "Current epoch\: [0-9]{6}$" | sed -E 's/Current epoch\: //g' | tr -d '\n'` # parse out the current epoch
+
 committeeMembers=$($ETHDO synccommittee members)
 read -r -a committeeMembersArray <<< "$committeeMembers"
 
 upcomingCommitteeMembers=$($ETHDO synccommittee members --period=next)
 read -r -a upcomingCommitteeMembersArray <<< "$upcomingCommitteeMembers"
 
-for index in "${validators[@]}"
+propserDutiesArray=($($ETHDO proposer duties --epoch $epoch)) # capture validator indexes that are proposing and split into an array - unfortunately adds a newline to the end of the validator index
+upcomingPropserDutiesArray=($($ETHDO proposer duties --epoch $(( epoch + 1 )) )) # capture validator indexes that are proposing in the next epoch and split into an array - unfortunately adds a newline to the end of the validator index
+
+# TODO: remove newline from proposer arrays to allow for exact match, instead of substring, below
+
+for validator in "${validators[@]}"
 do
+	for proposer in "${propserDutiesArray[@]}"
+	do
+		if [[ $proposer == *$validator* ]]; then # this is imperfect in that if a proposer index contains your index, it will be a false positive
+			echo "$validator" 'is going to propose a block this epoch!' | lolcat
+		fi
+	done
+	for proposer in "${upcomingPropserDutiesArray[@]}"
+	do
+		if [[ $proposer == *$validator* ]]; then # this is imperfect in that if a proposer index contains your index, it will be a false positive
+			echo "$validator" 'is going to propose a block NEXT epoch!' | lolcat
+		fi
+	done
 	for committeeMember in "${committeeMembersArray[@]}"
 	do
-		if [[ "$committeeMember" == "$index" ]]; then
-			echo "$index" 'is in a sync committee!' # woohoo! don't interrupt the staking machine!
+		if [[ "$committeeMember" == "$validator" ]]; then
+			echo "$validator" 'is in a sync committee!' | lolcat
 		fi
 	done
 	for upcomingCommitteeMember in "${upcomingCommitteeMembersArray[@]}"
 	do
-		if [[ "$upcomingCommitteeMember" == "$index" ]]; then
-			echo "$index" 'is coming up in the next sync committee!' # wow, congrats!
+		if [[ "$upcomingCommitteeMember" == "$validator" ]]; then
+			echo "$validator" 'is coming up in the next sync committee!' | lolcat
 		fi
 	done
 done
 
 echo '---'
 
-chainStatus=$($ETHDO chain status) # capture output of ethdo chain status to know when an epoch is ending
-epoch=`echo -e $chainStatus | grep -oEm 2 "Current epoch\: [0-9]{6}$" | sed -E 's/Current epoch\: //g' | tr -d '\n'` # parse out the current epoch
 NEXTSYNCCOMMITTEETIMESTAMP=$(( NOW + ((epoch % NUMEPOCHSINSYNCCOMMITTEE) * 32 * 12))) # calcuate the timestamp of the next sync committee
 echo $chainStatus # output ethdo's chain status
 syncCompletion1=`echo "scale=4; $(( epoch % NUMEPOCHSINSYNCCOMMITTEE )) / $NUMEPOCHSINSYNCCOMMITTEE * 100" | bc` # completion percent
